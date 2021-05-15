cecbe64f8422ea9279448345c8bf075ee6a8ad0e
Neeraj Srivastava <neerajsrv89@gmail.com>
2021-05-15 12:19:08 +0530

    my new webapp MVC

---
+++ NewWebAppDemo/NewWebAppDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NewWebAppDemo.Startup))]
+namespace NewWebAppDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
